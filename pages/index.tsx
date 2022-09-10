965f7c7e8848fc92a2083ef3e804a5c4c182e831
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-09-10 23:25:15 +0200

    progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { createElement, FC, useEffect, useState } from 'react'
+import React, { Component, createElement, FC, ReactElement, ReactNode, ReactPortal, useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 const Layout: FC<{ children: any }> = ({ children }) => {
@@ -55,7 +55,19 @@ const Container: FC<{ id: any, wrappers: any[], gap?: number }> = ({ id, wrapper
   )
 }
 
-const Wrapper: FC<{ component: any }> = ({ component }) => {
+// const isPageComponent = (arg: any): arg is PageComponent<PageComponentProps, any> => {
+//   console.log(typeof(arg.props));
+//   return arg && typeof(arg.props) == 'object' as string;
+// }
+interface WrapperInterface {
+  component: PageComponent
+}
+const Wrapper = ({ component }: WrapperInterface) => {
+  // if (isPageComponent(component)) {
+  //   console.log('component: ');
+  // } else {
+  //   throw Error('test');
+  // }
   return (
     <div>
       {component}
@@ -63,16 +75,21 @@ const Wrapper: FC<{ component: any }> = ({ component }) => {
   )
 }
 
-const Button: FC<{}> = ({ }) => {
-  return <button>asdas</button>
+interface PageComponent extends ReactElement { }
+interface PageComponentProps {
+  name: string
 }
 
-const DynamicCoponent: FC<{ComponentName: string}> = ({ComponentName}) => {
-  switch(ComponentName) {
-    case 'Button':
-      return <Button />
-  }
-  return <ComponentName />
+interface Test123 {
+  adsa: string
+}
+
+const Button = ({ name }: PageComponentProps): PageComponent => {
+  return <button>{name}</button>
+}
+
+const Button1 = ({ adsa }: Test123): ReactElement => {
+  return <div>asda</div>;
 }
 
 const Home: NextPage<{ ChoosenLayout: any }> = ({ ChoosenLayout }) => {
@@ -94,9 +111,10 @@ const Home: NextPage<{ ChoosenLayout: any }> = ({ ChoosenLayout }) => {
         <div className="flex">
           <div className="w-2/3 border">
             <Container id="left-column" gap={5} wrappers={[
-              <Wrapper component={<DynamicCoponent ComponentName={'Button'} />} />,
-              <Wrapper component={<Button />} />,
-              <Wrapper component={<Button />} />
+              <Wrapper key={uuidv4()} component={<Button name={'test123'} />} />,
+              <Wrapper key={uuidv4()} component={<Button1 adsa={'asdsa'} />} />,
+              // <Wrapper Component={<Button />} />,
+              // <Wrapper Component={<Button />} />
             ]} />
           </div>
           <div className="w-1/3 border">
@@ -110,6 +128,23 @@ const Home: NextPage<{ ChoosenLayout: any }> = ({ ChoosenLayout }) => {
 
 export async function getStaticProps(context: any) {
   const ChoosenLayout = 'Layout1';
+  // prototype
+  const config = {
+    "layout": {
+      "name": "Layout1",
+      "containers": {
+        "left-column": [
+          {
+            "props": {},
+            "component": {
+              "name": "x",
+              "props": {}
+            }
+          }
+        ]
+      }
+    }
+  };
   return {
     props: {
       ChoosenLayout: ChoosenLayout
