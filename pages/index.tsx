4b6faa8629b10c1418e32fd0656528eb535300a9
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-09-11 23:14:07 +0200

    progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,16 @@ import type { NextPage } from 'next'
 import React, { Component, createElement, FC, ReactElement, ReactNode, ReactPortal, useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
-const Layout: FC<{ children: any }> = ({ children }) => {
-  return (
-    <div className="container mx-auto h-screen">
-      <header className="h-1/4 border">
-        Header, Layout1
-      </header>
-      <main className="mt-5 mb-5">
-        {children}
-      </main>
-      <footer className="h-1/4 border">
-        footer
-      </footer>
-    </div>
-  )
+interface GeneralComponent {
+  id: string,
 }
 
-// const Layout2: FC<{ children: any }> = ({ children }) => {
-//   return (
-//     <div>
-//       <div>ala ma kota</div>
-//       <div>
-//         {children}
-//       </div>
-//     </div>
-//   )
-// }
-
-const Container: FC<{ id: any, wrappers: any[], gap?: number }> = ({ id, wrappers, gap = 5 }) => {
-  const [classes, setClasses] = useState('flex');
+interface ContainerInteface {
+  id: any,
+  wrappers: any[],
+  gap?: number
+}
+const Container = ({ id, wrappers, gap = 5 }: ContainerInteface) => {
   const gaps: any = {
     1: 'gap-1',
     2: 'gap-2',
@@ -38,11 +19,6 @@ const Container: FC<{ id: any, wrappers: any[], gap?: number }> = ({ id, wrapper
     4: 'gap-4',
     5: 'gap-5',
   };
-  useEffect(() => {
-    if (gap) {
-      setClasses(`flex ${gaps[gap]}`)
-    }
-  }, [gap])
 
   return (
     <div className={`flex ${gaps[gap]}`}>
@@ -54,28 +30,23 @@ const Container: FC<{ id: any, wrappers: any[], gap?: number }> = ({ id, wrapper
     </div>
   )
 }
-
-// const isPageComponent = (arg: any): arg is PageComponent<PageComponentProps, any> => {
-//   console.log(typeof(arg.props));
-//   return arg && typeof(arg.props) == 'object' as string;
-// }
-interface WrapperInterface {
-  component: PageComponent
+interface WrapperInterface extends GeneralComponent {
+  children: ReactElement<PageComponentProps>,
+  padding?: number,
 }
-const Wrapper = ({ component }: WrapperInterface) => {
-  // if (isPageComponent(component)) {
-  //   console.log('component: ');
-  // } else {
-  //   throw Error('test');
-  // }
+const Wrapper = ({ children, padding = 0 }: WrapperInterface) => {
+  const paddings: any = {
+    0: 'p-0',
+    1: 'p-1',
+    2: 'p-2',
+    3: 'p-3',
+  }
   return (
-    <div>
-      {component}
+    <div className={`border ${paddings[padding]}`}>
+      {children}
     </div>
   )
 }
-
-interface PageComponent extends ReactElement { }
 interface PageComponentProps {
   name: string
 }
@@ -84,7 +55,7 @@ interface Test123 {
   adsa: string
 }
 
-const Button = ({ name }: PageComponentProps): PageComponent => {
+const Button = ({ name }: PageComponentProps): ReactElement => {
   return <button>{name}</button>
 }
 
@@ -92,37 +63,43 @@ const Button1 = ({ adsa }: Test123): ReactElement => {
   return <div>asda</div>;
 }
 
-const Home: NextPage<{ ChoosenLayout: any }> = ({ ChoosenLayout }) => {
-  let FinalLayout = Layout;
-  switch (ChoosenLayout) {
-    case 'Layout1':
-      FinalLayout = Layout;
-      break;
-    case 'Layout2':
-      // FinalLayout = Layout2;
-      break;
-  }
+const Home: NextPage<{ ChoosenLayout: any, config: any }> = ({ ChoosenLayout, config }) => {
+  // config should be keep on state
+  // config will be editable by forms
   return (
-    <FinalLayout>
-      <div className="flex flex-col">
-        <div>
-          <Container id="top-bar" wrappers={[]} />
-        </div>
-        <div className="flex">
-          <div className="w-2/3 border">
-            <Container id="left-column" gap={5} wrappers={[
-              <Wrapper key={uuidv4()} component={<Button name={'test123'} />} />,
-              <Wrapper key={uuidv4()} component={<Button1 adsa={'asdsa'} />} />,
-              // <Wrapper Component={<Button />} />,
-              // <Wrapper Component={<Button />} />
-            ]} />
+    <div className={`container mx-auto h-screen bg-blue-500`}>
+      <header className="h-1/4 border">
+        Header, Layout1
+      </header>
+      <main className="mt-5 mb-5">
+        <div className="flex flex-col">
+          <div>
+            <Container id="top-bar" wrappers={[]} />
           </div>
-          <div className="w-1/3 border">
-            <Container id="side-bar" wrappers={[]} />
+          {/* <DynamicContainer id="top-baner-coś-tam"></DynamicContainer> */}
+          <div className="flex">
+            <div className="w-2/3 border">
+              <Container id="left-column" gap={5} wrappers={[
+                <Wrapper id="wrapper1" key={uuidv4()} padding={3}>
+                  <Button name={'test123'} />
+                </Wrapper>,
+                <Wrapper id="wrapper2" key={uuidv4()}>
+                  <Button1 adsa={'asdsa'} />
+                </Wrapper>,
+                // <Wrapper Component={<Button />} />,
+                // <Wrapper Component={<Button />} />
+              ]} />
+            </div>
+            <div className="w-1/3 border">
+              <Container id="side-bar" wrappers={[]} />
+            </div>
           </div>
         </div>
-      </div>
-    </FinalLayout>
+      </main>
+      <footer className="h-1/4 border">
+        footer
+      </footer>
+    </div>
   )
 }
 
@@ -130,24 +107,40 @@ export async function getStaticProps(context: any) {
   const ChoosenLayout = 'Layout1';
   // prototype
   const config = {
-    "layout": {
-      "name": "Layout1",
-      "containers": {
-        "left-column": [
-          {
-            "props": {},
-            "component": {
-              "name": "x",
-              "props": {}
-            }
-          }
-        ]
+    finalLayout: {
+      type: 'layout',
+      props: {
+        name: 'Layout1',
+        classes: 'bg-blue-500 p-5'
       }
-    }
+    },
+    "idComponentu": {
+      type: 'container',
+      props: {
+        wrappers: [
+          'wrapperId'
+        ],
+      },
+    },
+    wrapperId: {
+      type: 'wrapper',
+      props: {
+        padding: 'asda',
+        component: 'superComponentId',
+      }
+    },
+    superComponentId: {
+      type: 'component',
+      name: 'SuperCookie',
+      props: {
+
+      }
+    },
   };
   return {
     props: {
-      ChoosenLayout: ChoosenLayout
+      ChoosenLayout: ChoosenLayout,
+      config: config
     }, // will be passed to the page component as props
   }
 }
