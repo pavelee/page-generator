43ed54c8edb745607f7666a87764a5494d41f42a
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-09-23 12:33:43 +0200

    progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ const Modal = ({ isOpen = false, setIsOpen, children }: { isOpen: Boolean, setIs
 const EditWrapper = ({ children, form }: { children: any, form?: any }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="hover:outline hover:outline-1 relative hover:pt-5">
+    <div className="hover:outline hover:outline-1 relative pt-5">
       <button className="btn-xs absolute top-0" onClick={() => { setIsOpen(!isOpen) }}><AiOutlineTool /></button>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
         {form}
@@ -59,12 +59,10 @@ const EditWrapper = ({ children, form }: { children: any, form?: any }) => {
   );
 }
 
-const WrapperForm = () => {
-  const { config } = useConfig();
-  const { getProp, setProp } = UseForm(config);
+const WrapperForm = ({ componentId, setProp, getProp }: { componentId: string, setProp: any, getProp: any }) => {
   return (
-    <select value={getProp('wrapper1', 'padding')} onChange={(ev) => {
-      setProp('wrapper1', 'padding', ev.target.value)
+    <select value={getProp(componentId, 'padding')} onChange={(ev) => {
+      setProp(componentId, 'padding', ev.target.value)
     }}>
       <option value="0">0</option>
       <option value="1">1</option>
@@ -75,10 +73,14 @@ const WrapperForm = () => {
 }
 
 interface WrapperInterface extends GeneralComponent {
+  componentId: string,
   children: ReactElement<PageComponentProps>,
+  childrenForm: any,
+  getProp: any,
+  setProp: any,
   padding?: number,
 }
-const Wrapper = ({ children, form, padding = 3 }: WrapperInterface) => {
+const Wrapper = ({ componentId, children, getProp, setProp, childrenForm, padding = 3 }: WrapperInterface) => {
   const paddings: any = {
     0: 'p-0',
     1: 'p-1',
@@ -87,11 +89,11 @@ const Wrapper = ({ children, form, padding = 3 }: WrapperInterface) => {
   }
   return (
     <EditWrapper
-      form={<WrapperForm />}
+      form={<WrapperForm componentId={componentId} getProp={getProp} setProp={setProp} />}
     >
       <div className={`border ${paddings[padding]}`}>
         <EditWrapper
-          form={form}
+          form={childrenForm}
         >
           {children}
         </EditWrapper>
@@ -101,22 +103,37 @@ const Wrapper = ({ children, form, padding = 3 }: WrapperInterface) => {
 }
 
 interface PageComponentProps {
-  name: string
+  text: string
 }
 
-interface Test123 {
-  adsa: string
+const ButtonForm = ({ componentId, setProp, getProp }: { componentId: string, setProp: any, getProp: any }) => {
+  return (
+    <select value={getProp(componentId, 'type')} onChange={(ev) => {
+      setProp(componentId, 'type', ev.target.value)
+    }}>
+      <option value="standard">standard</option>
+      <option value="primary">primary</option>
+    </select>
+  )
 }
 
-const Button = ({ name }: PageComponentProps): ReactElement => {
-  return <div>
-    <button>{name}</button>
-    <a href="www.wip.pl">www.wip.pl</a>
-  </div>
+interface ButtonComponentInterface {
+  type?: string
 }
 
-const Button1 = ({ adsa }: Test123): ReactElement => {
-  return <div>asda</div>;
+const Button = ({ text, type }: PageComponentProps & ButtonComponentInterface): ReactElement => {
+  const [classes, setClasses] = useState('btn');
+  useEffect(() => {
+    switch (type) {
+      case 'primary':
+        setClasses('btn btn-primary');
+        break;
+      default:
+        setClasses('btn');
+    }
+  }, [type])
+
+  return <button className={`${classes}`}>{text}</button>
 }
 
 interface configurationProvider {
@@ -125,16 +142,11 @@ interface configurationProvider {
   configId: string,
 };
 
-const propsProvider = (config: { [key: string]: any }, configId: string) => {
-  return config[configId];
-}
-
-const getConfigValue = (config: { [key: string]: any }, id: string, key: string) => {
-  return config[id][key];
-}
-
 const UseForm = (config: any) => {
   const [conf, setConf] = useState(config);
+  const propsProvider = (configId: string) => {
+    return conf[configId];
+  }
   const getConfig = () => {
     return conf;
   }
@@ -142,9 +154,11 @@ const UseForm = (config: any) => {
     return conf[id][key];
   }
   const setProp = (id: string, key: string, value: string) => {
+    let ap = propsProvider(id);
     setConf(
       Object.assign({}, conf, {
         [id]: {
+          ...ap,
           [key]: value
         }
       })
@@ -152,10 +166,68 @@ const UseForm = (config: any) => {
   }
 
   return {
+    propsProvider,
     getConfig,
     getProp,
     setProp
   }
+}
+
+const Home: NextPage<{ editable: any, config: any }> = ({ config }) => {
+  const { getConfig, getProp, setProp, propsProvider } = UseForm(config);
+  // config should be keep on state
+  // config will be editable by forms
+  return (
+    <div className="container mx-auto h-screen bg-blue-500">
+      <header className="h-1/4 border">
+        Header, Layout1
+      </header>
+      <main className="mt-5 mb-5">
+        <div className="flex flex-col">
+          <div>
+            <Container {...propsProvider('top-bar')} />
+          </div>
+          {/* <DynamicContainer id="top-baner-coś-tam"></DynamicContainer> */}
+          <div className="flex">
+            <div className="w-2/3 border">
+              <Container gap={5} wrappers={[
+                <Wrapper
+                  componentId={'wrapper1'}
+                  childrenForm={
+                    <ButtonForm setProp={setProp} getProp={getProp} componentId={'button1'} />
+                  }
+                  getProp={getProp}
+                  setProp={setProp}
+                  {...propsProvider('wrapper1')}
+                  key={uuidv4()}>
+                  <Button {...propsProvider('button1')}
+                  />
+                </Wrapper>,
+                <Wrapper
+                  componentId={'wrapper2'}
+                  childrenForm={
+                    <ButtonForm setProp={setProp} getProp={getProp} componentId={'button2'} />
+                  }
+                  getProp={getProp}
+                  setProp={setProp}
+                  {...propsProvider('wrapper2')}
+                  key={uuidv4()}
+                >
+                  <Button {...propsProvider('button2')} />
+                </Wrapper>,
+              ]} />
+            </div>
+            <div className="w-1/3 border">
+              <Container wrappers={[]} />
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="h-1/4 border">
+        footer
+      </footer>
+    </div>
+  )
 }
 
 const useConfig = () => {
@@ -184,6 +256,14 @@ const useConfig = () => {
     wrapper2: { // to powinno być tworzone dynamiczne?
       padding: 3
     },
+    button1: {
+      text: 'ala ma kota',
+      type: 'standard',
+    },
+    button2: {
+      text: 'PROMOCJA',
+      type: 'standard'
+    },
     superComponentId: {
       type: 'component',
       name: 'SuperCookie',
@@ -195,96 +275,9 @@ const useConfig = () => {
   return { config }
 }
 
-const Home: NextPage<{ editable: any, config: any }> = ({ config }) => {
-  const { getConfig, getProp, setProp } = UseForm(config);
-  // config should be keep on state
-  // config will be editable by forms
-  return (
-    <div className="container mx-auto h-screen bg-blue-500">
-      <header className="h-1/4 border">
-        Header, Layout1
-      </header>
-      <main className="mt-5 mb-5">
-        <div className="flex flex-col">
-          <div>
-            <Container {...propsProvider(getConfig(), 'top-bar')} />
-          </div>
-          {/* <DynamicContainer id="top-baner-coś-tam"></DynamicContainer> */}
-          <div className="flex">
-            <div className="w-2/3 border">
-              {/* <select value={getConfigValue(conf, 'wrapper1', 'padding')} onChange={(ev) => {
-                setPadding('wrapper1', ev.target.value)
-              }}>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-              <select value={getConfigValue(conf, 'wrapper2', 'padding')} onChange={(ev) => {
-                setPadding('wrapper2', ev.target.value)
-              }}>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select> */}
-              <Container gap={5} wrappers={[
-                <Wrapper {...propsProvider(getConfig(), 'wrapper1')} key={uuidv4()}>
-                  <Button name={'test123'} />
-                </Wrapper>,
-                <Wrapper {...propsProvider(getConfig(), 'wrapper2')} key={uuidv4()}>
-                  <Button1 adsa={'asdsa'} />
-                </Wrapper>,
-              ]} />
-            </div>
-            <div className="w-1/3 border">
-              <Container wrappers={[]} />
-            </div>
-          </div>
-        </div>
-      </main>
-      <footer className="h-1/4 border">
-        footer
-      </footer>
-    </div>
-  )
-}
 
 export async function getStaticProps(context: any) {
   // prototype
-  const config1 = {
-    'top-bar': {
-      wrappers: []
-    },
-    finalLayout: {
-      type: 'layout',
-      props: {
-        name: 'Layout1',
-        classes: 'bg-blue-500 p-5'
-      }
-    },
-    "idComponentu": {
-      type: 'container',
-      props: {
-        wrappers: [
-          'wrapperId'
-        ],
-      },
-    },
-    wrapper1: {
-      padding: 3
-    },
-    wrapper2: { // to powinno być tworzone dynamiczne?
-      padding: 3
-    },
-    superComponentId: {
-      type: 'component',
-      name: 'SuperCookie',
-      props: {
-
-      }
-    },
-  };
   const { config } = useConfig();
   return {
     props: {
